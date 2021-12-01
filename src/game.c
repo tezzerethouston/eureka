5406c220fa5804f2515313482a8227673113dd77
tezzerethouston <tezzerethouston@users.noreply.github.com>
2021-12-02 00:46:05 +0100

    displays 2 boxed windows

--- src/game.c
+++ src/game.c
@@ -2,5 +2,27 @@
 #include "game.h"
 
 int	game (char a) {
+char	c;
+WINDOW	*spaceship, *dialog;
 
+spaceship = newwin(10, 30, 10, 1);
+dialog = newwin(6, 70, 20, 1);
+
+// === DISPLAY ===
+erase();
+refresh();
+box(spaceship, 0, 0);
+wrefresh(spaceship);
+box(dialog, 0, 0);
+wrefresh(dialog);
+// ===============
+
+while(1) { c=getch();
+switch(c) {
+	case 'q':
+		goto exit;
+}}
+
+exit:
+delwin(spaceship); delwin(dialog);
 return 0; }
